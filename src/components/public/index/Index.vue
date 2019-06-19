51562763372ddf03ffaddd75ceaef5b140088f7e
fracong <fracong@sina.cn>
2019-06-19 18:21:35 +0800

    使用element完成导航； 附【腾讯云element相关组件说明】：https://cloud.tencent.com/developer/chapter/18052

--- src/components/public/index/Index.vue
+++ src/components/public/index/Index.vue
@@ -2,7 +2,20 @@
   <div class="index">
     <div class="index-top">123</div>
     <div class="index-bottom">
-      <index-left-component :data="data"></index-left-component>
+      <el-row class="tac">
+        <el-col :span="15">
+          <index-left-component :data="data"></index-left-component>
+        </el-col>
+        <el-col :span="40">
+          <el-button>默认按钮</el-button>
+          <el-button type="primary">主要按钮</el-button>
+          <el-button type="success">成功按钮</el-button>
+          <el-button type="info">信息按钮</el-button>
+          <el-button type="warning">警告按钮</el-button>
+          <el-button type="danger">危险按钮</el-button>
+        </el-col>
+      </el-row>
+      <router-view></router-view>
     </div>
   </div>
 </template>
@@ -17,5 +30,11 @@ export default {
 </script>
 
 <style scoped>
-  @import './css/index.css'
+  @import './css/index.css';
+  .el-col-15{
+    width: 15%;
+  }
+  .el-col-40{
+      width: 40%;
+    }
 </style>
