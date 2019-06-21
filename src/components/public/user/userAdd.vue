bcb53afe018460494c5700a3532bda0e38481e37
fracong <fracong@sina.cn>
2019-06-21 16:02:26 +0800

    【用户新增页面】对话框的校验和重置操作： 关于prop：在校验和重置时，必须添加在el-form-item上，它的值和rules校验的key一致； 关于rules：只有在校验的时候才添加进来，必须添加在el-form标签上；

--- src/components/public/user/userAdd.vue
+++ src/components/public/user/userAdd.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <el-dialog title="新增用户" width="60%" class="dialog-title" :visible.sync="dialogFormVisible" :lock-scroll="lockScroll" :close-on-click-modal="closeOnClickModal">
-      <el-form :label-position="labelPosition" label-width="80px" ref="form" :model="form">
-        <el-form-item label="用户名">
-          <el-input v-model="form.name" class="input-width-35 input-right"></el-input>
+      <el-form :label-position="labelPosition" :rules="rules" label-width="80px" ref="form" :model="form">
+        <el-form-item label="用户名" prop="name">
+          <el-input v-model="form.name" class="input-width-35 input-right" ></el-input>
         </el-form-item>
-        <el-form-item label="地区">
-          <el-select class="input-right" v-model="form.region" placeholder="请选择地区">
+        <el-form-item label="地区" prop="region">
+          <el-select class="input-right" v-model="form.region" placeholder="请选择地区" >
             <el-option label="上海" value="shanghai"></el-option>
             <el-option label="北京" value="beijing"></el-option>
             <el-option label="广州" value="guangzhou"></el-option>
@@ -14,13 +14,13 @@
             <el-option label="武汉" value="wuhan"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="密码">
-          <el-input placeholder="请输入密码" class="input-width-35 input-right" v-model="form.password" show-password></el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input placeholder="请输入密码" class="input-width-35 input-right"  v-model="form.password" show-password></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        <el-button @click="closeUserAdd('form')">取 消</el-button>
+        <el-button type="primary" @click="submitForm('form')">确 定</el-button>
       </div>
     </el-dialog>
   </div>
@@ -30,19 +30,44 @@ export default {
   data() {
     return {
       dialogFormVisible: false,
-      lockScroll:false,
-      closeOnClickModal:false,
+      lockScroll:false,//不锁定底层页面的滚轮
+      closeOnClickModal:false,//点击底层页面，页面不关闭
       labelPosition: 'right',
       form: {
         name: '',
-        region: 'shenzhen',
+        region: '',
         password: '',
+      },
+      rules: {//校验的规则，prop上的值一致
+       name: [
+         { required: true, message: '请输入用户名', trigger: 'blur' },
+         { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' }
+       ],
+       region: [
+         { required: true, message: '请选择地区', trigger: 'change' }
+       ],
+       password: [
+         { type: 'string', required: true, message: '请填写密码', trigger: 'change' },
+         { min: 6, max: 32, message: '密码长度在 6 到 32 个字符', trigger: 'blur' }
+       ]
       }
     };
   },
   methods:{
-    resetFields(formName){
-      this.$refs[formName].resetFields();
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {//提交时进行校验
+        if (valid) {
+          alert('submit!');
+          this.dialogFormVisible = false;
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    closeUserAdd(formName){
+      this.$refs[formName].resetFields();//清空表单，清空的值包含prop属性，且prop属性必须标记在el-form-item上
+      this.dialogFormVisible = false;
     },
     clickDialogForm(){
       this.dialogFormVisible = true;
